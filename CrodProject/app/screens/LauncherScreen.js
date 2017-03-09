42ef1fd20288f02934bcaafdac880b46953bea01
Mohammad Jafarzadeh Rezvan <hulktroll@gmail.com>
2017-03-09 17:54:12 +0100

    login page

--- CrodProject/app/screens/LauncherScreen.js
+++ CrodProject/app/screens/LauncherScreen.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import {AppRegistry,StyleSheet,Text,View,TouchableOpacity, ListView} from 'react-native';
+import {StyleSheet,Text,View,TouchableOpacity, TextInput, Image} from 'react-native';
 import ViewContainer from '../components/ViewContainer.js'
-//import Icons from 'react-native-vector-icons/FontAwesome'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 
 class LauncherScreen extends Component {
@@ -15,10 +15,30 @@ class LauncherScreen extends Component {
 
   render() {
     return (
-      <ViewContainer>
-        <Text>
-          LauncherScreen
-        </Text>
+      <ViewContainer style={styles.container}>
+
+      <TextInput
+        placeholder="username"
+        placeholderTextColor="rgba(255,255,255,0.7)"
+        underlineColorAndroid="transparent"
+        style={styles.input}/>
+      <TextInput
+        placeholder="password"
+        placeholderTextColor="rgba(255,255,255,0.7)"
+        underlineColorAndroid="transparent"
+        style={styles.password}/>
+
+      <View style={styles.viewContainer}>
+        <TouchableOpacity style={styles.buttonContainer}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <Text style={styles.loginText}>Already a member</Text>
+      </View>
+      <View style={styles.circlesContent}>
+      <Icon name="circle" style={styles.circles}>
+      </Icon>
+      </View>
+
       </ViewContainer>
     )
   }
@@ -27,12 +47,57 @@ class LauncherScreen extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    padding: 20,
+    backgroundColor: '#024287',
+    justifyContent: 'center'
+  },
+  viewContainer: {
+    alignItems: 'center'
+  },
+  input: {
+    height: 45,
+    backgroundColor: 'rgba(255,255,255,0.2)',
+    marginBottom: 10,
+    color: '#FFF',
+    paddingHorizontal: 10,
+    fontSize: 18,
+    borderRadius: 8,
+  },
+  password: {
+    height: 45,
+    backgroundColor: 'rgba(255,255,255,0.2)',
+    marginBottom: 10,
+    color: '#FFF',
+    paddingHorizontal: 10,
+    fontSize: 18,
+    borderRadius: 8,
+    marginBottom: 30
+  },
+  buttonContainer: {
+    borderRadius: 50,
+    backgroundColor: 'white',
+    alignItems: 'center',
     justifyContent: 'center',
-    alignItems: 'stretch',
-    backgroundColor: '#F5FCFF',
-  }
+    width: 100,
+    height: 40,
+    marginBottom: 10
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#024287'
+  },
+  loginText: {
+    color: 'white'
+  },
+  circles: {
+    color: "white",
 
+  },
+  circlesContent: {
+    flex: 1,
+      justifyContent: "flex-end"
+  }
 });
 
 module.exports = LauncherScreen
