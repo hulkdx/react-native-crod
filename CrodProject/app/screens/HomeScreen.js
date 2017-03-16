46b077f170e2df4078a8b757c6ca943ee338cd2b
Mohammad Jafarzadeh Rezvan <hulktroll@gmail.com>
2017-03-16 16:42:31 +0100

    Home screen init

---
+++ CrodProject/app/screens/HomeScreen.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import {StyleSheet,Text} from 'react-native';
+
+
+class HomeScreen extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <Text>
+        Test
+      </Text>
+
+    )
+  }
+
+}
+
+const styles = StyleSheet.create({
+
+});
+
+module.exports = HomeScreen
--- CrodProject/index.android.js
+++ CrodProject/index.android.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {AppRegistry} from 'react-native';
 import LauncherScreen from './app/screens/LauncherScreen'
+import HomeScreen from './app/screens/HomeScreen'
 import {Router, Scene} from 'react-native-router-flux';
 
 class CrodProject extends Component {
@@ -9,7 +10,8 @@ class CrodProject extends Component {
     return (
       <Router>
         <Scene key='root'>
-          <Scene key='home' component={LauncherScreen} hideNavBar hideTabBar/>
+          <Scene key='home' component={HomeScreen} hideNavBar hideTabBar/>
+          <Scene key='launcher' component={LauncherScreen} hideNavBar hideTabBar/>
         </Scene>
       </Router>
     )
