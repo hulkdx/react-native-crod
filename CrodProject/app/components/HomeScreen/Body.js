192c759acaf4dabfeb353c2b2c48950b86295e41
Mohammad Jafarzadeh Rezvan <hulktroll@gmail.com>
2017-03-17 16:36:32 +0100

    Simple listview added to the body component

--- CrodProject/app/components/HomeScreen/Body.js
+++ CrodProject/app/components/HomeScreen/Body.js
@@ -1,28 +1,55 @@
 import React, { Component } from 'react';
-import {StyleSheet,TextInput,TouchableOpacity,Image,Text,View} from 'react-native';
+import {StyleSheet,TextInput,TouchableOpacity,Image,Text,View,ListView} from 'react-native';
+
+const proposals = [
+  {proposalName: "firstOne", proposalIcon: "firstOne"},
+  {proposalName: "two", proposalIcon: "firstOne"},
+  {proposalName: "firstOne", proposalIcon: "firstOne"},
+  {proposalName: "three", proposalIcon: "firstOne"},
+]
 
 class Body extends Component {
-  state = {
-    viewsTab: [],
-    isTabClosed: false,
-  };
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      viewsTab: [],
+      isTabClosed: false,
+      dataSource: ds.cloneWithRows(proposals),
+    };
+  }
 
   render() {
     return (
       <View style={{flex:1}}>
-        <Text >
-          FOOTER
-        </Text>
-
+        <ListView
+        dataSource={this.state.dataSource}
+        renderRow={(proposal) => {return this._renderProposalRow(proposal)} }
+        />
       </View>
 
 
     )
   }
 
+  _renderProposalRow(proposal){
+    return(
+      <View style={styles.rowProposalRoot}>
+        <Text>
+          {proposal.proposalIcon}
+        </Text>
+        <Text style={{marginRight: 40,}}>
+          {proposal.proposalName}
+        </Text>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
+  rowProposalRoot:{
+    flexDirection: 'row',
+  }
 });
 
 module.exports = Body
