293c2ef08dabdc43aa5a14f70180781d134fb40b
Mohammad Jafarzadeh Rezvan <hulktroll@gmail.com>
2017-03-20 11:02:55 +0100

    publish button added in Header and top bar organized

--- CrodProject/app/components/HomeScreen/Header.js
+++ CrodProject/app/components/HomeScreen/Header.js
@@ -13,32 +13,34 @@ class Header extends Component {
   render() {
     return (
       <View>
-        <TouchableOpacity disabled={this.state.isTabClosed} onPress={()=>this._dropdownOpen()} style={[styles.proposal, {flexDirection: 'row'}]}>
-          <Text style={[styles.createProposal, {flex: 1,}]}>Choose your proposal</Text>
-          <Text style={styles.publish}> Publish </Text>
+        <TouchableOpacity disabled={this.state.isTabClosed} onPress={()=>this._dropdownOpen()}
+          style={styles.topBarRoot}>
+          <View style={styles.titleProposalRoot}>
+            <Text style={styles.textProposal}>choose your proposal</Text>
+          </View>
+          <Image style={styles.publishImage} source={require('../../../img/publish-button.png')}/>
         </TouchableOpacity>
 
         {this.state.viewsTab}
 
       </View>
-
-
     )
   }
 
   _dropdownOpen(){
     this.setState({
       viewsTab:
-      <View style={[styles.proposal]}>
+      <View style={[styles.expandTab]}>
         <Text>catagory</Text>
         <TextInput
-          style={styles.createProposal}
+          style={styles.textInput}
           placeholder="create a title"/>
         <TextInput
-          style={styles.createProposal}
+          style={styles.textInput}
           placeholder="create a description"/>
         <Text>attachment</Text>
-        <TouchableOpacity onPress={()=>this._dropdownClose()}>
+        <TouchableOpacity
+        onPress={()=>this._dropdownClose()}>
           <Text>done</Text>
         </TouchableOpacity>
       </View>
@@ -55,13 +57,31 @@ class Header extends Component {
 
 const styles = StyleSheet.create({
   createProposal: {
+    flex:1,
     marginLeft: 50,
   },
-  proposal:{
-    backgroundColor: 'grey',
+  titleProposalRoot: {
+    flex: 4,
+    alignItems: 'center',
+    paddingTop: 20,
+    paddingBottom: 20,
   },
-  publish: {
-    marginRight: 30
+  expandTab: {
+    backgroundColor: '#b6b6b6',
+  },
+  topBarRoot:{
+    flexDirection: 'row',
+    backgroundColor: '#b6b6b6',
+  },
+  publishImage: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'contain',
+    marginRight: 5,
+  },
+  textInput: {
+
   }
 });
 
