0d1a0b7b8d3f77128787b6986a00dcad7d40e7f1
Brigel Pineti <brigelpineti@gmail.com>
2017-03-21 16:57:48 +0100

    Body class divided in CategoryMenu & ProposalFeed Left Category side-menu created for the home screen.

--- CrodProject/app/components/HomeScreen/Body.js
+++ CrodProject/app/components/HomeScreen/Body.js
@@ -1,55 +1,27 @@
 import React, { Component } from 'react';
-import {StyleSheet,TextInput,TouchableOpacity,Image,Text,View,ListView} from 'react-native';
+import {Dimensions, ScrollView, PropTypes, StyleSheet,TextInput,TouchableOpacity,Image,Text,View,ListView} from 'react-native';
+import CategoryMenu from "./CategoryMenu.js"
+import ProposalFeed from "./ProposalFeed.js"
 
-const proposals = [
-  {proposalName: "firstOne", proposalIcon: "firstOne"},
-  {proposalName: "two", proposalIcon: "firstOne"},
-  {proposalName: "firstOne", proposalIcon: "firstOne"},
-  {proposalName: "three", proposalIcon: "firstOne"},
-]
+const LeftSideMenu = require('react-native-side-menu');
 
 class Body extends Component {
-  constructor() {
-    super();
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    this.state = {
-      viewsTab: [],
-      isTabClosed: false,
-      dataSource: ds.cloneWithRows(proposals),
-    };
-  }
-
   render() {
+    const menu = <CategoryMenu />;
+
     return (
       <View style={{flex:8}}>
-        <ListView
-        dataSource={this.state.dataSource}
-        renderRow={(proposal) => {return this._renderProposalRow(proposal)} }
-        />
+      <LeftSideMenu menu={menu}>
+      <ProposalFeed />
+      </LeftSideMenu>
       </View>
 
-
-    )
-  }
-
-  _renderProposalRow(proposal){
-    return(
-      <View style={styles.rowProposalRoot}>
-        <Text>
-          {proposal.proposalIcon}
-        </Text>
-        <Text style={{marginRight: 40,}}>
-          {proposal.proposalName}
-        </Text>
-      </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
-  rowProposalRoot:{
-    flexDirection: 'row',
-  }
 });
+
 
 module.exports = Body
