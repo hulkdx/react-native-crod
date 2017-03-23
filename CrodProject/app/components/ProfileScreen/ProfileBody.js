5bd82de89652e0e1d5135289ae31d25e10b07dae
Mohammad Jafarzadeh Rezvan <hulktroll@gmail.com>
2017-03-23 13:01:27 +0100

    Tabs and Body from homescreen added to profile body

--- CrodProject/app/components/ProfileScreen/ProfileBody.js
+++ CrodProject/app/components/ProfileScreen/ProfileBody.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import {StyleSheet,TextInput,TouchableOpacity,Image,Text,View,ListView} from 'react-native';
-
-
+import Tabs from 'react-native-tabs';
+import Body from '../HomeScreen/Body.js'
 
 class ProfileBody extends Component {
 
+  constructor(props){
+      super(props);
+      this.state = {page:'proposal'};
+    }
 
   render() {
     return (
-      <View style={{flex:8}}>
-        <Text> Profile Body </Text>
+      <View style={styles.container}>
+        <Tabs selected={this.state.page}
+            onSelect={el=>this.setState({page:el.props.name})}
+            style={styles.tabs}>
+            <Text name="proposal"
+                  selectedIconStyle={styles.tabsSelected}>Your Proposals</Text>
+            <Text name="history"
+                  selectedIconStyle={styles.tabsSelected}>History votes </Text>
+        </Tabs>
+        <Body />
+
       </View>
 
 
@@ -19,6 +32,16 @@ class ProfileBody extends Component {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex:8,
+    backgroundColor: '#ADDBE5',
+  },
+  tabs: {
+    position:'relative',
+  },
+  tabsSelected: {
+    backgroundColor: 'white',
+  },
 });
 
 module.exports = ProfileBody
