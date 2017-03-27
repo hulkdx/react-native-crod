c2bca4baac22c46491fe7f444e9a6cb8a937bfb1
Mohammad Jafarzadeh Rezvan <hulktroll@gmail.com>
2017-03-27 15:47:20 +0200

    id added to proposal.js

--- CrodProject/app/data-manager/proposals.js
+++ CrodProject/app/data-manager/proposals.js
@@ -1,9 +1,9 @@
 const proposals = [
-  {title: "Reduce parking lot in 20% in favor of bicycle paking", deadline: "1.5.2017", voteYes: 10, voteNo: 5},
-  {title: "increase spending on CSR", deadline: "2.5.2017", voteYes: 10, voteNo: 5},
-  {title: "firstOne", deadline: "3.5.2017", voteYes: 10, voteNo: 5},
-  {title: "three"   , deadline: "4.5.2017", voteYes: 10, voteNo: 5},
-  {title: "three"   , deadline: "5.5.2017", voteYes: 10, voteNo: 5},
+  {id:0, title: "Reduce parking lot in 20% in favor of bicycle paking", deadline: "1.5.2017", voteYes: 10, voteNo: 5},
+  {id:1, title: "increase spending on CSR", deadline: "2.5.2017", voteYes: 10, voteNo: 5},
+  {id:2, title: "firstOne", deadline: "3.5.2017", voteYes: 10, voteNo: 5},
+  {id:3, title: "three"   , deadline: "4.5.2017", voteYes: 10, voteNo: 5},
+  {id:4, title: "three"   , deadline: "5.5.2017", voteYes: 10, voteNo: 5},
 ]
 
 module.exports = proposals
