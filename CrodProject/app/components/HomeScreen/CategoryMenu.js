d28a439d10d0fd2ef54fc945cf1bf425126471d2
Brigel Pineti <brigelpineti@gmail.com>
2017-04-24 16:46:17 +0200

    CategoryMenu.js: categories were given checkboxes behaviour || When clicked the color of the corrensponding image & text is changed

--- CrodProject/app/components/HomeScreen/CategoryMenu.js
+++ CrodProject/app/components/HomeScreen/CategoryMenu.js
@@ -6,11 +6,16 @@ import {Actions} from 'react-native-router-flux';
 
 class CategoryMenu extends Component {
 
-
-
-
+  constructor(props) {
+  super(props)
+  this.state = {
+    //Initialy, isSelected contains an array of false values.
+    isSelected: Array(categories.length).fill(false)
+  }
+}
 
   render() {
+
 
     return (
 
@@ -20,12 +25,13 @@ class CategoryMenu extends Component {
     );
   }
   _renderCategoryImages(){
+    var isSelected= []
     var rows = [];
     for (let i=0; i<categories.length; i++){
       rows.push(
         <TouchableOpacity key = {i} onPress={this._onClickCategoryImage.bind(this,i)} style={styles.categoryDropDown}>
-            <Image source={categories[i].image} style={styles.categoriesLeftSide}/>
-            <Text style={styles.categoryNameTxt}> {categories[i].name} </Text>
+            <Image source={categories[i].image} style={[styles.categoriesLeftSide, {tintColor: this.state.isSelected[i] ? '#88B3D9' : 'white' }]}/>
+            <Text style={[styles.categoryNameTxt, {color: this.state.isSelected[i] ? '#88B3D9' : 'white' }]}> {categories[i].name} </Text>
         </TouchableOpacity>
 
       )
@@ -38,8 +44,11 @@ class CategoryMenu extends Component {
     )
   }
   _onClickCategoryImage = (id) => {
+    //Toggle property: when clicked from false => true && true => false
+    this.state.isSelected[id]= !this.state.isSelected[id]
+
     //When clicked the id of the category icon is passed as a prop to the HomeScreen through Actions.refresh.
-    Actions.refresh({key: 'home', categoryId: id})
+    Actions.refresh()
   }
 
 };
--- CrodProject/app/data-manager/categories.js
+++ CrodProject/app/data-manager/categories.js
@@ -1,26 +1,37 @@
 
 
 const categories = [
-    { name: "Environment",
+    { id: 0,
+      name: "Environment",
       image: require("../../img/categories/environment.png"),
-      icon: require("../../img/categories/science_small.png")
+      icon: require("../../img/categories/science_small.png"),
+      checked: false,
     },
-    { name: "Financial",
+    { id: 1,
+      name: "Financial",
       image: require("../../img/categories/financial.png"),
-      icon: require("../../img/categories/science_small.png")
+      icon: require("../../img/categories/science_small.png"),
+      checked: false,
     },
     {
+      id: 2,
       name: "Politics",
       image: require("../../img/categories/politics.png"),
-      icon: require("../../img/categories/science_small.png")
+      icon: require("../../img/categories/science_small.png"),
+      checked: false,
     },
-    { name: "Science",
+    {
+      id: 3,
+      name: "Science",
       image: require("../../img/categories/science.png"),
-      icon: require("../../img/categories/science_small.png")
+      icon: require("../../img/categories/science_small.png"),
+      checked: false,
     },
-    { name: "Social",
+    { id: 4,
+      name: "Social",
       image: require("../../img/categories/social.png"),
-      icon: require("../../img/categories/science_small.png")
+      icon: require("../../img/categories/science_small.png"),
+      checked: false,
     },
 ]
 
