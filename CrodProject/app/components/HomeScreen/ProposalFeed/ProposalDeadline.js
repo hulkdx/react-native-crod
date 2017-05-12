78c6a706ff11f4cd66777aaf1daa031f206d7722
Mohammad Jafarzadeh Rezvan <hulktroll@gmail.com>
2017-05-12 12:16:24 +0200

    styles orginized in proposalDeadline

--- CrodProject/app/components/HomeScreen/ProposalFeed/ProposalDeadline.js
+++ CrodProject/app/components/HomeScreen/ProposalFeed/ProposalDeadline.js
@@ -11,21 +11,21 @@ class ProposalDeadline extends Component {
 
   render() {
     return (
-      <View style={styles.deadlineRoot}>
+      <View style={styles.container}>
 
-      <View style={styles.daysLeft}>
-        <Text style={{color: '#DC143C', textAlign: 'center', fontSize: 16.5}}>2 days</Text>
+      <View style={styles.daysLeftContainer}>
+        <Text style={styles.daysLeft}>2 days</Text>
       </View>
 
 
-      <View style={styles.day}>
-        <Text style={{color: '#5d95c4', fontSize: 25, textAlign: 'center',}}> {this.props.day} </Text>
+      <View style={styles.dayContainer}>
+        <Text style={styles.day}> Sun </Text>
       </View>
 
-      <View style={{height: 1, backgroundColor: '#5d95c4'}} />
+      <View style={styles.divider} />
 
-      <View style={styles.date}>
-        <Text style={{color: '#5d95c4', textAlign: 'center',}}> {this.props.month} {this.props.date} </Text>
+      <View style={styles.dateContainer}>
+        <Text style={styles.date}> Apr 16 </Text>
       </View>
 
       </View>
@@ -34,27 +34,7 @@ class ProposalDeadline extends Component {
 }
 
 const styles = StyleSheet.create({
-  daysLeft:{
-    flex:1,
-    justifyContent: 'center',
-    padding: 5,
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-  },
-  day:{
-    flex:1,
-    justifyContent: 'center',
-    padding: 5
-  },
-  date:{
-    flex:1,
-    justifyContent: 'center',
-    borderColor: '#5d95c4',
-    borderBottomWidth: 0.7,
-    borderBottomLeftRadius: 10,
-    borderBottomRightRadius: 10,
-  },
-  deadlineRoot: {
+  container: {
     flex: 1,
     height: 100,
     justifyContent: 'center',
@@ -62,6 +42,44 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     borderRadius: 10,
   },
+  daysLeftContainer:{
+    flex:1,
+    justifyContent: 'center',
+    padding: 5,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  daysLeft: {
+    color: '#DC143C',
+    textAlign: 'center',
+    fontSize: 16.5
+  },
+  dayContainer: {
+    flex:1,
+    justifyContent: 'center',
+    padding: 5
+  },
+  day: {
+    color: '#5d95c4',
+    fontSize: 25,
+    textAlign: 'center',
+  },
+  divider: {
+    height: 1,
+    backgroundColor: '#5d95c4'
+  },
+  dateContainer: {
+    flex:1,
+    justifyContent: 'center',
+    borderColor: '#5d95c4',
+    borderBottomWidth: 0.7,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+  },
+  date: {
+    color: '#5d95c4',
+    textAlign: 'center',
+  }
 });
 
 module.exports = ProposalDeadline
