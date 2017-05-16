5a06875f9111987b1426449585ce986cb8d452be
Mohammad Jafarzadeh Rezvan <hulktroll@gmail.com>
2017-05-16 15:47:53 +0200

    redux boilerplate for categories

--- CrodProject/app/components/HomeScreen/CategoryMenu.js
+++ CrodProject/app/components/HomeScreen/CategoryMenu.js
@@ -3,84 +3,76 @@
 
   Categories listed in SideMenu
 */
-'use strict'
+'use strict';
 import React, { Component } from 'react';
-import {ScrollView, StyleSheet,TouchableOpacity,Image,Text,View} from 'react-native';
-import categories from "../../data-manager/categories.js"
-import {Actions} from 'react-native-router-flux';
-
+import { ScrollView, StyleSheet, TouchableOpacity, Image, Text, View } from 'react-native';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import categories from '../../data-manager/categories.js';
+import * as categoryActions from '../../reducers/categories/categoryActions';
 
 class CategoryMenu extends Component {
 
   constructor(props) {
-  super(props)
-  this.state = {
+    super(props);
+    this.state = {
     //Initialy, isSelected contains an array of false values.
-    isSelected: Array(categories.length).fill(false)
+    isSelected: Array(categories.length).fill(false) };
   }
-}
 
   render() {
-
-
     return (
-
       <ScrollView scrollsToTop={false} style={styles.categories}>
-      {this._renderCategoryImages()}
+      {this.renderCategoryImages()}
       </ScrollView>
     );
   }
-  _renderCategoryImages(){
-    var isSelected= []
-    var rows = [];
-    for (let i=0; i<categories.length; i++){
+
+  renderCategoryImages() {
+    const rows = [];
+    for (let i = 0; i < categories.length; i++) {
       rows.push(
-        <TouchableOpacity key = {i} onPress={this._onClickCategoryImage.bind(this,i)} style={[styles.categoryDropDown, {borderRightWidth: this.state.isSelected[i] ? 2 : 0} ]}>
+        <TouchableOpacity key={i} onPress={this._onClickCategoryImage.bind(this, i)} style={[styles.categoryDropDown, { borderRightWidth: this.state.isSelected[i] ? 2 : 0 }]}>
           {this.state.isSelected[i] ?
             <Image source={categories[i].imageFill} style={styles.categoriesLeftSide} /> :
-            <Image source={categories[i].image} style={styles.categoriesLeftSide}/> }
+            <Image source={categories[i].image} style={styles.categoriesLeftSide} />
+          }
             <Text style={styles.categoryNameTxt}> {categories[i].name} </Text>
         </TouchableOpacity>
-
-      )
+      );
     }
     return (
       <View>
         {rows}
-
       </View>
-    )
+    );
   }
+
   _onClickCategoryImage = (id) => {
     //Toggle property: when clicked from false => true && true => false
-    this.state.isSelected[id]= !this.state.isSelected[id]
-
-    //When clicked the id of the category icon is passed as a prop to the HomeScreen through Actions.refresh.
-    Actions.refresh()
+    this.state.isSelected[id] = !this.state.isSelected[id];
   }
 
-};
+}
 
 
 const styles = StyleSheet.create({
-  rowProposalRoot:{
+  rowProposalRoot: {
     flexDirection: 'row',
   },
-  categories:{
+  categories: {
     flexDirection: 'column',
   },
-  categoryDropDown:{
+  categoryDropDown: {
     paddingTop: 5,
     marginTop: 25,
-    alignItems:'center',
+    alignItems: 'center',
     justifyContent: 'center',
     borderColor: '#ffeb3b',
-    //borderColor: '#5d95c4',
     borderRightWidth: 2
 
   },
-  categoriesLeftSide:{
-
+  categoriesLeftSide: {
     tintColor: '#ffeb3b',
     height: 50,
     width: 50,
@@ -95,5 +87,14 @@ const styles = StyleSheet.create({
   }
 });
 
-
-module.exports = CategoryMenu
+// Redux boilerplate
+function mapStateToProps(state) {
+  return {
+    category: state.category,
+  };
+}
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(categoryActions, dispatch);
+}
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryMenu);
+// module.exports = CategoryMenu
