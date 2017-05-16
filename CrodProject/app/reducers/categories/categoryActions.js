64eb6578e44da1dabdadb1fb5ff3805e4423c4e4
Mohammad Jafarzadeh Rezvan <hulktroll@gmail.com>
2017-05-16 16:05:57 +0200

    ESLint

--- CrodProject/app/reducers/categories/categoryActions.js
+++ CrodProject/app/reducers/categories/categoryActions.js
@@ -3,60 +3,59 @@
 
   Actions
 */
-'use strict'
-import { api } from '../../lib/Api'
-import { getToken } from '../auth/authActions'
+'use strict';
+import { api } from '../../lib/Api';
+import { getToken } from '../auth/authActions';
 // import store from 'react-native-simple-store'
 const {
   GET_CATEGORY_REQUEST,
   GET_CATEGORY_SUCCESS,
   GET_CATEGORY_FAILURE,
-} = require('../../lib/constants').default
+} = require('../../lib/constants').default;
 
 /*############## Get category Section ##############*/
-export function getCategories () {
+export function getCategories() {
   return dispatch => {
-    dispatch(categoryRequest ())
+    dispatch(categoryRequest());
     return getToken()
     .then((token) => {
-      return api.getCategories(token)
-      .then(function (json) {
-        dispatch(categorySuccess(json))
+      api.getCategories(token)
+      .then((json) => {
+        dispatch(categorySuccess(json));
       })
       .catch((error) => {
         if (error === 'unauth') {
           // TODO LOG OUT, REMOVE TOKEN
           // Actions.login({type: ActionConst.REFRESH})
         }
-        dispatch(categoryFailure(error))
-      })
+        dispatch(categoryFailure(error));
+      });
     })
     .catch((error) => {
-      dispatch(categoryFailure(error))
+      dispatch(categoryFailure(error));
       // Actions.login({type: ActionConst.REFRESH})
-    })
-
-  }
+    });
+  };
 }
 
-export function categoryRequest () {
+export function categoryRequest() {
   return {
     type: GET_CATEGORY_REQUEST
-  }
+  };
 }
 
-export function categorySuccess (json) {
+export function categorySuccess(json) {
   return {
     type: GET_CATEGORY_SUCCESS,
     payload: json
-  }
+  };
 }
 
-export function categoryFailure (error) {
+export function categoryFailure(error) {
   return {
     type: GET_CATEGORY_FAILURE,
     payload: error
-  }
+  };
 }
 
 /*############## TODO Create Category Section ##############*/
