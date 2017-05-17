a015214e8ba2c7c14eb03a35071b1439c527e94c
Mohammad Jafarzadeh Rezvan <hulktroll@gmail.com>
2017-05-17 14:57:31 +0200

    ESLint

--- CrodProject/app/reducers/proposals/proposalsActions.js
+++ CrodProject/app/reducers/proposals/proposalsActions.js
@@ -3,10 +3,10 @@
 
   Actions
 */
-'use strict'
+'use strict';
 import { Actions, ActionConst } from 'react-native-router-flux';
-import { api } from '../../lib/Api'
-import { getToken } from '../auth/authActions'
+import { api } from '../../lib/Api';
+import { getToken } from '../auth/authActions';
 // import store from 'react-native-simple-store'
 const {
   PROPOSAL_REQUEST,
@@ -15,94 +15,93 @@ const {
   CREATE_PROPOSAL_REQUEST,
   CREATE_PROPOSAL_SUCCESS,
   CREATE_PROPOSAL_FAILURE,
-} = require('../../lib/constants').default
+} = require('../../lib/constants').default;
 
 /*############## Get Proposals Section ##############*/
-export function getProposals () {
+export function getProposals() {
   return dispatch => {
-    dispatch(proposalsRequest ())
+    dispatch(proposalsRequest());
     return getToken()
     .then((token) => {
       return api.getProposals(token)
-      .then(function (json) {
-        dispatch(proposalsSuccess(json))
+      .then((json) => {
+        dispatch(proposalsSuccess(json));
       })
       .catch((error) => {
         if (error === 'unauth') {
           // TODO LOG OUT, REMOVE TOKEN
-          Actions.login({type: ActionConst.REFRESH})
+          Actions.login({ type: ActionConst.REFRESH });
         }
-        dispatch(proposalsFailure(error))
-      })
+        dispatch(proposalsFailure(error));
+      });
     })
     .catch((error) => {
-      dispatch(proposalsFailure(error))
-      Actions.login({type: ActionConst.REFRESH})
-    })
-
-  }
+      dispatch(proposalsFailure(error));
+      Actions.login({ type: ActionConst.REFRESH });
+    });
+  };
 }
 
-export function proposalsRequest () {
+export function proposalsRequest() {
   return {
     type: PROPOSAL_REQUEST
-  }
+  };
 }
 
-export function proposalsSuccess (json) {
+export function proposalsSuccess(json) {
   return {
     type: PROPOSAL_SUCCESS,
     payload: json
-  }
+  };
 }
 
-export function proposalsFailure (error) {
+export function proposalsFailure(error) {
   return {
     type: PROPOSAL_FAILURE,
     payload: error
-  }
+  };
 }
 
 /*############## Create Proposal Section ##############*/
-export function createProposal (categoryId, title, desc, deadline) {
+export function createProposal(categoryId, title, desc, deadline) {
   return dispatch => {
-    dispatch(createProposalRequest())
+    dispatch(createProposalRequest());
     return getToken()
     .then((token) => {
       return api.createProposals(token, categoryId, title, desc, deadline)
-      .then(function (json) {
-        dispatch(createProposalSuccess(json))
+      .then((json) => {
+        dispatch(createProposalSuccess(json));
       })
       .catch((error) => {
         if (error === 'unauth') {
           // TODO LOG OUT, REMOVE TOKEN
-          Actions.login({type: ActionConst.REFRESH})
+          Actions.login({ type: ActionConst.REFRESH });
         }
-        dispatch(proposalsFailure(error))
-      })
+        dispatch(proposalsFailure(error));
+      });
     })
     .catch((error) => {
-      dispatch(createProposalFailure(error))
-    })
-  }
+      dispatch(createProposalFailure(error));
+    });
+  };
 }
 
 export function createProposalRequest() {
   return {
     type: CREATE_PROPOSAL_REQUEST
-  }
+  };
 }
 
-export function createProposalSuccess (json) {
+export function createProposalSuccess(json) {
   return {
     type: CREATE_PROPOSAL_SUCCESS,
     payload: json
-  }
+  };
 }
 
-export function createProposalFailure (error) {
+export function createProposalFailure(error) {
   return {
     type: CREATE_PROPOSAL_FAILURE,
     payload: error
-  }
+  };
 }
