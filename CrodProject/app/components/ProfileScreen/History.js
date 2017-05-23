aadb9a87a2a837e92a62e4e65405e44917e303b8
Mohammad Jafarzadeh Rezvan <hulktroll@gmail.com>
2017-05-23 14:21:38 +0200

    Profile screen updates with the api

--- CrodProject/app/components/ProfileScreen/History.js
+++ CrodProject/app/components/ProfileScreen/History.js
@@ -2,25 +2,56 @@
   Created by Mohammad Jafarzadeh Rezvan, Brigel Pineti
 
   ProfileHistory tab
+  TODO do this tab.
 */
 'use strict';
 import React, { Component } from 'react';
 import { StyleSheet, TouchableOpacity, View, ListView } from 'react-native';
 import { Actions, ActionConst } from 'react-native-router-flux';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as proposalsActions from '../../reducers/proposals/proposalsActions';
 import ProposalTitle from '../HomeScreen/ProposalFeed/ProposalTitle.js';
-import userProposal from '../../data-manager/userHistory';
-// TODO
-const categorySource = require('../../../img/categories/science.png');
 
 class ProfileHistory extends Component {
 
 
   constructor(props) {
     super(props);
-    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
     this.state = {
-      dataSource: ds.cloneWithRows(userProposal),
+      dataSource: new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 }),
+      dataSourceUpdated: false,
     };
+  }
+
+  /*
+    Recive the user proposals when components mounted
+    TODO: (change codes to commented code) make a user api and get
+          user proposals instead of filtering the fetched proposals
+  */
+  componentDidMount() {
+    // Proposals updated successfully from api
+    // console.log();
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows(this.props.proposals.proposals.filter(
+        (proposal) => { return proposal.user.is_your_proposal === true; })),
+      dataSourceUpdated: true });
+
+  //   if (this.props.proposals.proposalsUpdated) {
+  //     return;
+  //   }
+  //   this.props.getUserProposals();
+  // }
+  //
+  // componentWillReceiveProps(nextProps) {
+  //   // update data source with this.props.proposals
+  //   if (this.props.proposals.proposalsUpdated) {
+  //     return;
+  //   }
+  //   if (nextProps.proposals.proposals.length === 0) return;
+  //     //console.log('not updated');
+  //     this.setState({ dataSource: this.state.dataSource.cloneWithRows(nextProps.proposals.proposals),
+  //                    dataSourceUpdated: true });
   }
 
   render() {
@@ -43,7 +74,12 @@ class ProfileHistory extends Component {
     return (
       <View style={styles.rowContainer}>
       <TouchableOpacity style={styles.rowProposalRoot} onPress={this.proposalClicked.bind(this, proposal)}>
-        <ProposalTitle fullName={proposal.fullName} profilePic={proposal.profilePic} text={proposal.title} category={categorySource} />
+      <ProposalTitle text={proposal.title}
+                     articles={proposal.articles}
+                     discussions={proposal.discussions}
+                     categoryIcon={proposal.category_source_fill}
+                     deadline={proposal.deadline}
+      />
       </TouchableOpacity>
       </View>
     );
@@ -78,5 +114,13 @@ const styles = StyleSheet.create({
   },
 });
 
-
-module.exports = ProfileHistory;
+// Redux boilerplate
+function mapStateToProps(state) {
+  return {
+    proposals: state.proposals,
+  };
+}
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(proposalsActions, dispatch);
+}
+export default connect(mapStateToProps, mapDispatchToProps)(ProfileHistory);
