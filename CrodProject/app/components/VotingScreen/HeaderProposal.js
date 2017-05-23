377f0b4d0b3d4463464f44bffe687157c0b4cb63
Mohammad Jafarzadeh Rezvan <hulktroll@gmail.com>
2017-05-23 15:09:22 +0200

    Left and right arrow working

--- CrodProject/app/components/VotingScreen/HeaderProposal.js
+++ CrodProject/app/components/VotingScreen/HeaderProposal.js
@@ -9,25 +9,45 @@ const backgroundImage = require('../../../img/backgroundvotingup.jpg');
 
 class Proposal extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      currentProposal: 0,
+    };
+  }
+
   render() {
     // Check if it is clicked on a proposal or it is clicked on a bottom bar votting
-    const title = this.props.proposalId === -1 ? this.props.proposals.proposals[0].title
+    const title = this.props.proposalId === -1 ? this.props.proposals.proposals[this.state.currentProposal].title
     : this.props.proposals.proposals.filter((x) => { return x.id === this.props.proposalId; })[0].title;
     return (
       <Image style={styles.container} source={backgroundImage}>
-
-        <TouchableOpacity>
+      {this.props.proposalId === -1 &&
+        <TouchableOpacity onPress={this.onArrowClicked.bind(this, false)}>
           <Icon style={styles.arrow} name="arrow-left" size={35} />
         </TouchableOpacity>
-
+      }
         <Text style={styles.title}>{title}</Text>
-
-        <TouchableOpacity>
+      {this.props.proposalId === -1 &&
+        <TouchableOpacity onPress={this.onArrowClicked.bind(this, true)}>
           <Icon style={styles.arrow} name="arrow-right" size={35} />
         </TouchableOpacity>
-
+      }
       </Image>
     );
+  }
+
+  // @param cond: if left arrow left clicked = false; right arrow = true
+  // decrease state.currentProposa upon cond=false and increase otherwise
+  onArrowClicked = (cond) => {
+    console.log(this.props.proposals.proposals.length);
+    if (cond) {
+      if (this.state.currentProposal === this.props.proposals.proposals.length - 1) return;
+      this.setState({ currentProposal: this.state.currentProposal + 1 });
+    } else {
+      if (this.state.currentProposal === 0) return;
+      this.setState({ currentProposal: this.state.currentProposal - 1 });
+    }
   }
 
 }
